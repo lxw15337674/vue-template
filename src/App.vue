c4d9a6b1b140639df5306703af23208ae09b0385
15515255978 <404174262@qq.com>
2020-06-30 16:16:59 +0800

    prettier 配置修改

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="home">
-    this is app
-  </div>
+    <div class="home">
+        this is app
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data() {
-    return {};
-  },
-  created() {},
-  methods: {},
+    name: 'app',
+    data() {
+        return {};
+    },
+    created() {},
+    methods: {},
 };
 </script>
 <style lang="stylus" scoped></style>
